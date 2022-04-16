772fa2c86d871341d3d46c5f9e04a7bf3c530410
GarikD <garikd@outlook.fr>
2022-04-16 14:29:03 +0200

    Add : User delete route

--- controllers/usersController.ts
+++ controllers/usersController.ts
@@ -1,13 +1,34 @@
+import { UsersService } from "./../services/usersService.ts";
 import { Router } from "https://deno.land/x/oak@v10.5.1/mod.ts";
 
 export class UsersController {
-  private usersRouter = new Router()
-    .get("/", (ctx) => {
-      ctx.response.body = "This returns all users";
-    })
-    .get("/:id", (ctx) => {
-      ctx.response.body = `This returns a user with the id ${ctx.params.id}`;
-    });
+  private usersService: UsersService;
+  private usersRouter: Router;
+
+  constructor() {
+    this.usersService = new UsersService();
+    this.usersRouter = new Router();
+    this.initRoutes();
+  }
+
+  private initRoutes() {
+    this.usersRouter
+      .get("/", (ctx) => {
+        ctx.response.body = this.usersService.getUsers();
+      })
+      .get("/:id", (ctx) => {
+        ctx.response.body = this.usersService.getUser(Number(ctx.params.id));
+      })
+      .delete("/:id", (ctx) => {
+        this.usersService.deleteUser(Number(ctx.params.id));
+        ctx.response.body = null;
+      })
+      .post("/", async (ctx) => {
+        const { value } = ctx.request.body({ type: "json" });
+        const user = await value;
+        this.usersService.addUser(user);
+      });
+  }
 
   getRouter() {
     return this.usersRouter;
